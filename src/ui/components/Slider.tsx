bbf057603eee570b590a4b9173f8f36b141eeb99
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2025-10-13 22:20:45 +0200

    feat: Redesign slider component and use icons (#59)
    Adds MUI icons for play/stop and the volume slider. The slider is also
    redesigned to match with the button next to it.

--- src/ui/components/Slider.tsx
+++ src/ui/components/Slider.tsx
@@ -1,20 +1,30 @@
 import { FunctionComponent, PropsWithChildren } from 'react'
+import './Slider.css'
 
 export const Slider: FunctionComponent<PropsWithChildren<{
   min: number
   max: number
   value: number
   onChange: (value: number) => void
-}>> = ({ min, max, value, onChange }) => {
+  label?: string
+  step?: number
+}>> = ({ children, min, max, value, onChange, label, step }) => {
   return (
-    <input
-      type='range'
-      min={min}
-      max={max}
-      value={value}
-      step='any'
-      onChange={(event) => onChange(event.currentTarget.valueAsNumber)}
-      className='w-32'
-    />
+    <label
+      className='px-3 py-1 h-10 leading-none rounded flex items-center gap-3 border-2 border-gray-600 text-white select-none'
+      title={label}
+    >
+      {children}
+
+      <input
+        type='range'
+        min={min}
+        max={max}
+        value={value}
+        step={step}
+        onChange={(event) => onChange(event.currentTarget.valueAsNumber)}
+        className='w-32 cadence-slider'
+      />
+    </label>
   )
 }
