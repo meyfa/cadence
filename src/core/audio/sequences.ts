19e96104aefcc655a952fa92b0af0b6015a76224
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2025-10-14 19:19:11 +0200

    feat: Support multiple routings per instrument
    This patch lifts the requirement that per section, only one pattern may be
    routed to an instrument. As an example, the following is now possible:
    
    ```
    section main for 4 bars {
      kick << [x--- ----]
      kick << [---- x---]
    }
    ```
    
    Both routings would create their own independent sequences, in effect playing
    the kickdrum once per bar.
    
    How overlapping step information is handled would depend on the instrument.

--- src/core/audio/sequences.ts
+++ src/core/audio/sequences.ts
@@ -1,59 +1,46 @@
 import { Sequence, Player } from 'tone'
-import { getSilentPattern, withPatternLength } from '../pattern.js'
+import { withPatternLength } from '../pattern.js'
 import { convertPitchToPlaybackRate } from '../midi.js'
 import { isPitch, type Instrument, type InstrumentId, type Program, type Step } from '../program.js'
 
 export const DEFAULT_ROOT_NOTE = 'C5' as const
 
-export function createSequences (players: Map<InstrumentId, Player>, program: Program): Map<InstrumentId, Sequence<Step>> {
+export type SequenceWithOffset = [sequence: Sequence<Step>, offset: number]
+
+export function createSequences (players: Map<InstrumentId, Player>, program: Program): readonly SequenceWithOffset[] {
   const subdivision = `${program.beatsPerBar * program.stepsPerBeat}n`
-  const sequenceEvents = createEvents(players, program)
 
-  const sequences = new Map<InstrumentId, Sequence<Step>>()
+  const sequences: SequenceWithOffset[] = []
 
-  for (const [key, player] of players) {
-    const events = sequenceEvents.get(key)
-    const instrument = program.instruments.get(key)
+  const timePerStep = 60 / (program.stepsPerBeat * program.track.tempo.value)
+  let currentOffsetSteps = 0
 
-    if (events != null && instrument != null) {
-      sequences.set(key, new Sequence<Step>({
+  for (const section of program.track.sections) {
+    const startOffset = currentOffsetSteps * timePerStep
+    currentOffsetSteps += section.length.value
+
+    for (const routing of section.routings) {
+      const instrument = program.instruments.get(routing.instrumentId)
+      const player = players.get(routing.instrumentId)
+
+      if (instrument == null || player == null) {
+        continue
+      }
+
+      // For some reason, Tone.js wants a mutable events array.
+      const events = [...withPatternLength(routing.pattern, section.length.value)]
+      const sequence = new Sequence<Step>({
         callback: createCallback(instrument, player),
         events,
         subdivision,
         loop: false
-      }))
+      })
+
+      sequences.push([sequence, startOffset])
     }
   }
 
   return sequences
-}
-
-function createEvents (players: Map<InstrumentId, Player>, program: Program): Map<InstrumentId, Step[]> {
-  const instrumentEvents = new Map<InstrumentId, Step[]>([
-    ...Array.from(players.keys(), (id): [InstrumentId, Step[]] => [id, []])
-  ])
-
-  for (const section of program.track.sections) {
-    const used = new Set<InstrumentId>()
-
-    for (const routing of section.routings) {
-      const player = players.get(routing.instrumentId)
-      const events = instrumentEvents.get(routing.instrumentId)
-
-      if (!used.has(routing.instrumentId) && player != null && events != null) {
-        events.push(...withPatternLength(routing.pattern, section.length.value))
-        used.add(routing.instrumentId)
-      }
-    }
-
-    for (const [key, events] of instrumentEvents) {
-      if (!used.has(key)) {
-        events.push(...getSilentPattern(section.length.value))
-      }
-    }
-  }
-
-  return instrumentEvents
 }
 
 function createCallback (instrument: Instrument, player: Player): (time: number, note: Step) => void {
