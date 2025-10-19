adaf240e3061e33c578720eb4fc518efd14251c2
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2025-10-19 21:37:53 +0200

    feat: Replace gray color classes with neutral (#80)
    TailwindCSS's "gray" has a blueish tone that I don't particularly enjoy. The
    app certainly needs some actual color design, but right now, it seems reasonable
    to use a neutral gray for most things.

--- packages/app/src/components/Footer.tsx
+++ packages/app/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import type { FunctionComponent, PropsWithChildren } from 'react'
 
 export const Footer: FunctionComponent<PropsWithChildren> = ({ children }) => {
   return (
-    <footer className='flex px-4 py-1 gap-2 text-sm text-gray-400 border-t border-t-gray-700 items-start'>
+    <footer className='flex px-4 py-1 gap-2 text-sm text-neutral-400 border-t border-t-neutral-700 items-start'>
       {children}
     </footer>
   )
--- packages/app/src/components/Header.tsx
+++ packages/app/src/components/Header.tsx
@@ -1,8 +1,7 @@
+import { PlayArrowOutlined, StopOutlined, VolumeDownOutlined, VolumeOffOutlined, VolumeUpOutlined } from '@mui/icons-material'
 import { FunctionComponent } from 'react'
 import { Button } from './Button.js'
 import { Slider } from './Slider.js'
-import { PlayArrowOutlined, StopOutlined, VolumeDownOutlined, VolumeOffOutlined, VolumeUpOutlined } from '@mui/icons-material'
-import clsx from 'clsx'
 
 export const Header: FunctionComponent<{
   playing: boolean
@@ -12,10 +11,7 @@ export const Header: FunctionComponent<{
   progress?: number
 }> = ({ playing, onPlayPause, volume, onVolumeChange, progress }) => {
   return (
-    <header className={clsx(
-      'w-full border-b border-b-gray-700 flex flex-wrap items-center px-4 py-1 gap-2'
-    )}
-    >
+    <header className='w-full border-b border-b-neutral-600 flex flex-wrap items-center px-4 py-1 gap-2'>
       <div className='text-lg font-semibold mr-2'>
         Cadence
       </div>
@@ -30,7 +26,7 @@ export const Header: FunctionComponent<{
 
       {progress != null && (
         <div title='Progress' className='grow min-w-24 max-w-64 flex items-center h-8'>
-          <div className='w-full h-2 bg-gray-600 rounded-xs overflow-hidden'>
+          <div className='w-full h-2 bg-neutral-600 rounded-xs overflow-hidden'>
             <div
               className='h-full bg-white transition-all duration-100 ease-linear'
               style={{ width: `${(progress * 100).toFixed(2)}%` }}
