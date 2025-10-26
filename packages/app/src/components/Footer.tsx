a60e0386b82763fcb18792ce99d67d99d6c5a605
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2025-10-26 12:15:13 +0100

    refactor: Refactor footer component
    Due to the new layout system, Footer and EditorFooter no longer need to be
    separate components. Additionally, this patch prepares for clickable footer items
    (error count; caret location), but this is not used yet, as it requires a refactoring
    of application state first.

--- packages/app/src/components/Footer.tsx
+++ packages/app/src/components/Footer.tsx
@@ -1,9 +1,42 @@
+import type { EditorLocation } from '@editor/editor.js'
+import type { RangeError } from '@language/error.js'
 import type { FunctionComponent, PropsWithChildren } from 'react'
+import { pluralize } from '../utilities/strings.js'
 
-export const Footer: FunctionComponent<PropsWithChildren> = ({ children }) => {
+export const Footer: FunctionComponent<{
+  errors: readonly RangeError[]
+  editorLocation?: EditorLocation
+  onClickErrors?: () => void
+  onClickLocation?: () => void
+}> = ({ errors, editorLocation, onClickErrors, onClickLocation }) => {
   return (
-    <footer className='flex px-4 py-1 gap-2 text-sm text-content-200 border-t border-t-frame-100 items-start'>
-      {children}
+    <footer className='flex h-6 px-2 gap-2 items-center text-sm bg-surface-200 text-content-200 border-t border-t-frame-100 select-none'>
+      <FooterButton onClick={onClickErrors}>
+        {errors.length === 0 ? 'No errors' : pluralize(errors.length, 'error')}
+      </FooterButton>
+
+      <div className='flex-1' />
+
+      {editorLocation != null && (
+        <FooterButton onClick={onClickLocation}>
+          Ln {editorLocation.line}, Col {editorLocation.column}
+        </FooterButton>
+      )}
     </footer>
+  )
+}
+
+const FooterButton: FunctionComponent<PropsWithChildren<{
+  onClick?: () => void
+}>> = ({ onClick, children }) => {
+  return (
+    <button
+      type='button'
+      className='px-2 h-full enabled:cursor-pointer enabled:hocus:bg-surface-300 enabled:hocus:text-content-300'
+      onClick={onClick}
+      disabled={onClick == null}
+    >
+      {children}
+    </button>
   )
 }
