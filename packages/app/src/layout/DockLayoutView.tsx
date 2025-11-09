762720ea4d53208aefb3ceb85647dccce5056ec8
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2025-11-09 12:37:50 +0100

    feat: Preview tab DnD abstractly instead of recomputing layout (#125)
    We no longer compute a full preview layout during drag-and-drop of tabs.
    Firstly, this is somewhat expensive, and secondly, it gets complicated
    once splitting and merging of panes is introduced. Instead, show a line
    marker where the tab will end up. This is also what VS Code does, and it
    works nicely.

--- packages/app/src/layout/DockLayoutView.tsx
+++ packages/app/src/layout/DockLayoutView.tsx
@@ -1,5 +1,5 @@
 import { closestCenter, DndContext, DragOverlay, MouseSensor, useSensor, useSensors, type DragEndEvent, type DragStartEvent } from '@dnd-kit/core'
-import { findPaneByTabId, moveTabBetweenPanes, moveTabBetweenPanesPreview } from '@editor/layout/layout.js'
+import { findPaneByTabId, moveTabBetweenPanes } from '@editor/layout/layout.js'
 import type { DockLayout, Tab, TabId } from '@editor/state/layout.js'
 import clsx from 'clsx'
 import { useCallback, useState, type FunctionComponent } from 'react'
@@ -18,7 +18,6 @@ export const DockLayoutView: FunctionComponent<{
   // Passed down to not call for every tab pane
   const tabRendererContext = useTabRendererContext()
 
-  const [dragPreviewLayout, setDragPreviewLayout] = useState<DockLayout | undefined>(undefined)
   const [draggedTab, setDraggedTab] = useState<Tab | undefined>(undefined)
 
   const mouseSensor = useSensor(MouseSensor, {
@@ -30,48 +29,31 @@ export const DockLayoutView: FunctionComponent<{
   const sensors = useSensors(mouseSensor)
 
   const onDragStart = useCallback(({ active }: DragStartEvent) => {
-    setDragPreviewLayout(layout)
     const activeId = active.id.toString() as TabId
     const sourcePane = findPaneByTabId(layout, activeId)
     setDraggedTab(sourcePane?.tabs.find((tab) => tab.id === activeId))
   }, [layout])
 
   const onDragOver = useCallback(({ active, over }: DragEndEvent) => {
-    if (over != null && active.id !== over.id) {
-      const activeId = active.id.toString() as TabId
-      const overId = over.id.toString() as TabId
-      setDragPreviewLayout((previewLayout) => {
-        if (previewLayout == null) {
-          return previewLayout
-        }
-        return moveTabBetweenPanesPreview(previewLayout, activeId, overId)
-      })
-    }
+    // TODO show split preview
   }, [])
 
   const finalizeDrag = useCallback(() => {
     // Delay resetting drag state as HeadlessUI may still mess with focus during the same tick
     queueMicrotask(() => {
-      setDragPreviewLayout(undefined)
       setDraggedTab(undefined)
     })
   }, [])
 
   const onDragEnd = useCallback(({ active, over }: DragEndEvent) => {
-    if (dragPreviewLayout != null) {
-      dispatch(() => {
-        if (over != null && active.id !== over.id) {
-          const activeId = active.id.toString() as TabId
-          const overId = over.id.toString() as TabId
-          return moveTabBetweenPanes(dragPreviewLayout, activeId, overId)
-        }
-
-        return dragPreviewLayout
-      })
-    }
+    dispatch((layout) => {
+      const activeId = active.id.toString() as TabId
+      const overId = (over ?? active).id.toString() as TabId
+      return moveTabBetweenPanes(layout, activeId, overId)
+    })
 
     finalizeDrag()
-  }, [dispatch, finalizeDrag, dragPreviewLayout])
+  }, [dispatch, finalizeDrag])
 
   return (
     <DndContext
@@ -83,13 +65,11 @@ export const DockLayoutView: FunctionComponent<{
       onDragEnd={onDragEnd}
     >
       <div className={clsx('flex flex-col', className)}>
-        {dragPreviewLayout != null
-          ? renderNode(dragPreviewLayout.main, tabRendererContext, undefined)
-          : renderNode(layout.main, tabRendererContext, mainDispatch)}
+        {renderNode(layout.main, tabRendererContext, draggedTab != null ? undefined : mainDispatch)}
       </div>
 
       {draggedTab != null && (
-        <DragOverlay dropAnimation={null}>
+        <DragOverlay dropAnimation={null} className='opacity-60'>
           <TabComponent tab={draggedTab} context={tabRendererContext} disabled={true} selected={true} />
         </DragOverlay>
       )}
