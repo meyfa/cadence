606b543003e25701e3b00f7a31edefb3464aa8cb
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-01-11 14:20:39 +0000

    refactor: Add and use `useSafeContext` hook
    This enables removal of the `undefined as any` code smell, in favor of
    explicit guards in case the context provider is missing at the callsite.

--- packages/app/src/state/AudioEngineContext.tsx
+++ packages/app/src/state/AudioEngineContext.tsx
@@ -1,9 +1,9 @@
 import type { AudioEngine } from '@core/audio/engine.js'
-import { createContext, useContext } from 'react'
+import { createContext } from 'react'
+import { useSafeContext } from '../hooks/context.js'
 
-// This will be mounted by the top-level App component, so for simplicity, we don't provide a fallback here.
-export const AudioEngineContext = createContext<AudioEngine>(undefined as any)
+export const AudioEngineContext = createContext<AudioEngine | undefined>(undefined)
 
 export function useAudioEngine (): AudioEngine {
-  return useContext(AudioEngineContext)
+  return useSafeContext(AudioEngineContext, 'AudioEngineContext')
 }
