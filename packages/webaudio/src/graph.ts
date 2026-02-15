78e657194a719628fd6db717c5f4feb293a66b4a
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-02-15 11:55:36 +0000

    feat: Implement offline audio renderer
    Adds the capability to render audio to an offline buffer instead of live
    playback, which will be used for exporting (in a follow-up patch). To
    make this possible, some of the logic has been factored out into a new
    graph module, which can be shared between the session and renderer. Some
    of the error handling logic has been updated for simplicity.

---
+++ packages/webaudio/src/graph.ts
@@ -0,0 +1,78 @@
+import type { Numeric, Program } from '@core/program.js'
+import type { AudioFetcher } from './assets/fetcher.js'
+import { createBuses } from './buses.js'
+import type { BaseMixin } from './instances.js'
+import { createInstruments } from './instruments.js'
+import { scheduleNoteEvents } from './parts.js'
+import { setupRoutings } from './routings.js'
+import type { Transport } from './transport.js'
+
+export const ErrorMessages = Object.freeze({
+  LoadTimeout: 'Timeout while loading assets.',
+  Unknown: 'An unknown error occurred during audio rendering.'
+})
+
+export interface AudioGraph {
+  /**
+   * Resolves once all instances have finished their load attempts.
+   * Rejects if any instance fails to load or the timeout is reached.
+   */
+  readonly loaded: Promise<void>
+
+  readonly dispose: () => void
+  readonly disposed: boolean
+}
+
+export function createAudioGraph (
+  transport: Transport,
+  program: Program,
+  fetcher: AudioFetcher,
+  timeout: Numeric<'s'>
+): AudioGraph {
+  const buses = createBuses(transport, program)
+  const instruments = createInstruments(transport, program, fetcher)
+
+  const instances: BaseMixin[] = [
+    ...buses.values(),
+    ...instruments.values()
+  ]
+
+  setupRoutings(transport, program, instruments, buses)
+  scheduleNoteEvents(program, instruments)
+
+  let timeoutId: ReturnType<typeof setTimeout> | undefined
+  const timeoutPromise = new Promise((resolve, reject) => {
+    timeoutId = setTimeout(() => {
+      reject(new Error(ErrorMessages.LoadTimeout))
+    }, timeout.value * 1000)
+  })
+
+  const loaded = Promise.race([
+    timeoutPromise,
+    Promise.all(instances.map(async (item) => await item.loaded))
+  ])
+    .then(() => undefined)
+    .finally(() => clearTimeout(timeoutId))
+
+  let disposed = false
+
+  return {
+    loaded,
+
+    get disposed () {
+      return disposed
+    },
+
+    dispose: () => {
+      if (disposed) {
+        return
+      }
+
+      disposed = true
+      clearTimeout(timeoutId)
+      for (const item of instances) {
+        item.dispose()
+      }
+    }
+  }
+}
