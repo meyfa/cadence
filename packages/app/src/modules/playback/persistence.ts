8447fa8819f90dc20ce1b2cf5f8e5e5a9a4e6219
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-04-08 20:35:26 +0000

    fix: Rename timeline module to playback module
    The module is fully responsible for playback-related features, so the
    new name is more fitting. The timeline panel is just one part of the
    playback user interface.

--- packages/app/src/modules/playback/persistence.ts
+++ packages/app/src/modules/playback/persistence.ts
@@ -8,27 +8,27 @@ import { validateNumeric } from '../../utilities/validation.js'
 
 export const defaultOutputGain = numeric('db', -12)
 
-interface TimelineSettings {
+interface PlaybackSettings {
   readonly outputGain: Numeric<'db'>
 }
 
-const timelineSettingsSchema: Struct<TimelineSettings> = type({
+const playbackSettingsSchema: Struct<PlaybackSettings> = type({
   outputGain: validateNumeric('db')
 })
 
-const timelineSettingsDefaults: TimelineSettings = {
+const playbackSettingsDefaults: PlaybackSettings = {
   outputGain: defaultOutputGain
 }
 
-export const outputGainDomain: PersistenceDomain<TimelineSettings> = {
-  key: 'timeline',
-  fallbackValue: timelineSettingsDefaults,
+export const outputGainDomain: PersistenceDomain<PlaybackSettings> = {
+  key: 'playback',
+  fallbackValue: playbackSettingsDefaults,
   serialize: (value) => value,
-  deserialize: (value) => timelineSettingsSchema.create(value),
+  deserialize: (value) => playbackSettingsSchema.create(value),
   areEqual: (a, b) => a.outputGain.value === b.outputGain.value
 }
 
-export function useTimelineSettingsSync (): void {
+export function usePlaybackSettingsSync (): void {
   const audioEngine = useAudioEngine()
   const outputGain = useObservable(audioEngine.outputGain)
 
