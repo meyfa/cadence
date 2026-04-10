0e6759d5c264684d6e5592142c93af809b513848
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-04-10 19:09:15 +0200

    refactor: Move editor theme to app package (#410)
    The theme contains specific colors and styles that should not generally
    be enforced, but instead be provided as customization by the app.

--- packages/editor/src/editor/types.ts
+++ packages/editor/src/editor/types.ts
@@ -1,6 +1,8 @@
-export type EditorTheme = 'dark' | 'light'
+import type { Transaction, TransactionSpec } from '@codemirror/state'
 
 export interface EditorLocation {
   readonly line: number
   readonly column: number
 }
+
+export type EditorViewDispatch = ((transaction: Transaction) => void) & ((...specs: TransactionSpec[]) => void)
