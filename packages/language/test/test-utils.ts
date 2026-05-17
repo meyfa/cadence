e6a4028cd68be7b094bbf6376554ede3544217c6
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-05-17 07:47:19 +0000

    test: Shorten parser tests
    1. Assume the lexer produces correct tokens, as covered by lexer tests.
        This allows us to use source strings in parser tests instead of
        hand-crafting verbose token arrays.
    2. Shorten assertions to check primarily the AST structure rather than
        the full return value hierarchy, unless relevant to the test case.

--- packages/language/test/test-utils.ts
+++ packages/language/test/test-utils.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-type-parameters */
 
+import type { Result, SuccessResult } from '../src/result/result.js'
+import assert from 'node:assert'
+
 export type Equals<X, Y> =
   (<T>() => T extends X ? 1 : 2) extends
   (<T>() => T extends Y ? 1 : 2) ? true : false
@@ -7,4 +10,10 @@ export type Equals<X, Y> =
 // Passes typechecking iff types X and Y are exactly equal (no arguments)
 export function expectTypeEquals<X, Y> (..._args: [Equals<X, Y>] extends [true] ? [] : ['Types are not equal']): void {
   // no-op
+}
+
+export function assertResultComplete<TValue, TError extends Error> (
+  result: Result<TValue, TError>
+): asserts result is SuccessResult<TValue> {
+  assert.ok(result.complete, result.complete ? undefined : result.error)
 }
