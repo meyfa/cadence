119d6bf5f49217eb493bb89b488b1c04b56fd1a7
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-05-24 21:46:19 +0200

    fix: Handle already disposed state in DisposeStack (#503)

--- packages/utility/src/disposable/disposable.ts
+++ packages/utility/src/disposable/disposable.ts
@@ -6,16 +6,28 @@ export interface Disposable {
 
 export class DisposeStack implements Disposable {
   private items: Dispose[] = []
+  private disposed = false
 
   push (dispose: Dispose): void {
+    if (this.disposed) {
+      dispose()
+      return
+    }
+
     this.items.push(dispose)
   }
 
   pushDisposable (disposable: Disposable): void {
-    this.items.push(disposable.dispose.bind(disposable))
+    this.push(disposable.dispose.bind(disposable))
   }
 
   dispose (): void {
+    if (this.disposed) {
+      return
+    }
+
+    this.disposed = true
+
     while (this.items.length > 0) {
       this.items.pop()?.()
     }
