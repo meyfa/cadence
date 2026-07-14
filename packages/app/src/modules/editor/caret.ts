d4340b97fe1d9abb727505a039a7ac20d175e9bc
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-07-14 11:45:11 +0200

    chore: Remove dependency on tsx (#619)
    Use Node.js native TypeScript support for running tests instead of tsx.
    
    There is an edge-case in "app". The unit under test imports from editor
    which exports React components. While not relevant for the test itself,
    Node.js still prevents it from running. As a workaround, the test is
    migrated to Vitest.

--- packages/app/src/modules/editor/caret.ts
+++ packages/app/src/modules/editor/caret.ts
@@ -1,7 +1,7 @@
 import type { DockLayout, EditorLocation, PanelId } from '@meyfa/cadence-editor'
 import { findFocusedTab } from '@meyfa/cadence-editor'
 import { getEditorPanelProps } from './panel-props.ts'
-import type { EditorState } from './provider.ts'
+import type { EditorState } from './provider.tsx'
 
 export function getFocusedEditorFilePath (layout: DockLayout, panelId: PanelId): string | undefined {
   const tab = findFocusedTab(layout)
