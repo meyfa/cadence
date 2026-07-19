0a8a5b454ce6ce5734079d09e6e464df57780da6
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-07-19 08:48:45 +0200

    refactor: Reorder AST types (#640)

--- packages/ast/src/ast.ts
+++ packages/ast/src/ast.ts
@@ -1,56 +1,99 @@
 import type { StepValue } from '@meyfa/cadence-core'
 import type { SourceRange } from './range.ts'
 
+// Abstract Type
+
 export interface ASTNode {
   readonly type: string
   readonly range: SourceRange
 }
 
-// Basic Types
+// Factory
 
-export interface Identifier extends ASTNode {
-  readonly type: 'Identifier'
-  readonly name: string
+export function make<T extends keyof NodeByType> (
+  type: T,
+  range: SourceRange,
+  props: Omit<NodeByType[T], 'type' | 'range'>
+): NodeByType[T] {
+  return {
+    ...(props as any),
+
+    // must come last to override props
+    type,
+    range
+  } as NodeByType[T]
 }
 
-export interface Number extends ASTNode {
-  readonly type: 'Number'
-  readonly value: number
+export interface NodeByType {
+  // Root Type
+  Program: Program
+
+  // Building Blocks
+  Import: Import
+  Statement: Statement
+  Identifier: Identifier
+
+  // Expressions
+  UnaryExpression: UnaryExpression
+  BinaryExpression: BinaryExpression
+  PropertyAccess: PropertyAccess
+  Call: Call
+  Property: Property
+
+  // Primitive Types
+  Number: Number
+  String: String
+  Pattern: Pattern
+  Step: Step
+  Curve: Curve
+  CurveSegment: CurveSegment
+
+  // Constructed Types
+  Mixer: Mixer
+  Bus: Bus
+  Track: Track
+  Part: Part
+  Instrument: Instrument
+  Voice: Voice
+
+  // Combination Types
+  Routing: Routing
+  Automation: Automation
 }
 
-export interface String extends ASTNode {
-  readonly type: 'String'
-  readonly parts: ReadonlyArray<string | Expression>
+export type AnyNode = NodeByType[keyof NodeByType]
+
+export type Value =
+  Identifier |
+  Number |
+  String |
+  Pattern |
+  Curve |
+  Mixer |
+  Bus |
+  Track |
+  Part |
+  Instrument |
+  Voice |
+  Routing |
+  Automation
+
+export type Expression =
+  Value |
+  UnaryExpression |
+  BinaryExpression |
+  PropertyAccess |
+  Call
+
+// Root Type
+
+export interface Program extends ASTNode {
+  readonly type: 'Program'
+  readonly imports: readonly Import[]
+  readonly children: readonly Statement[]
 }
 
-export interface Step extends ASTNode {
-  readonly type: 'Step'
-  readonly value: StepValue
-  readonly length?: Expression
-  readonly parameters: ArgumentList
-}
-
-type PatternMode = 'serial' | 'parallel'
-
-export interface Pattern extends ASTNode {
-  readonly type: 'Pattern'
-  readonly mode: PatternMode
-  readonly children: ReadonlyArray<Step | Expression>
-}
-
-export interface CurveSegment extends ASTNode {
-  readonly type: 'CurveSegment'
-  readonly curveType: string
-  readonly parameters: readonly Expression[]
-  readonly length: Expression
-}
-
-export interface Curve extends ASTNode {
-  readonly type: 'Curve'
-  readonly children: ReadonlyArray<CurveSegment | Expression>
-}
-
-// Imports
+// Building Blocks
 
 export interface Import extends ASTNode {
   readonly type: 'Import'
@@ -60,52 +103,6 @@ export interface Import extends ASTNode {
    * The alias to use for the imported library. If undefined, imports all symbols directly.
    */
   readonly alias?: string
-}
-
-// Expression Types
-
-export const unaryOperators = ['+', '-'] as const
-export type UnaryOperator = typeof unaryOperators[number]
-
-export interface UnaryExpression extends ASTNode {
-  readonly type: 'UnaryExpression'
-  readonly operator: UnaryOperator
-  readonly argument: Expression
-}
-
-export const binaryOperators = ['+', '-', '*', '/'] as const
-export type BinaryOperator = typeof binaryOperators[number]
-
-export interface BinaryExpression extends ASTNode {
-  readonly type: 'BinaryExpression'
-  readonly operator: BinaryOperator
-  readonly left: Expression
-  readonly right: Expression
-}
-
-export interface PropertyAccess extends ASTNode {
-  readonly type: 'PropertyAccess'
-  readonly object: Expression
-  readonly property: Identifier
-}
-
-export interface Call extends ASTNode {
-  readonly type: 'Call'
-  readonly callee: Expression
-  readonly arguments: ReadonlyArray<Expression | Property>
-}
-
-export type Value = Identifier | Number | String | Pattern | Curve | Instrument | Voice | Mixer | Bus | Track | Part | Routing | Automation
-export type Expression = Value | UnaryExpression | BinaryExpression | PropertyAccess | Call
-
-// Composite Types
-
-export type ArgumentList = ReadonlyArray<Expression | Property>
-
-export interface Property extends ASTNode {
-  readonly type: 'Property'
-  readonly key: Identifier
-  readonly value: Expression
 }
 
 export type Statement = NamedStatement | UnnamedStatement
@@ -126,7 +123,90 @@ interface UnnamedStatement extends ASTNode {
   readonly values: readonly [Expression, ...Expression[]]
 }
 
-// Domain Types
+export interface Identifier extends ASTNode {
+  readonly type: 'Identifier'
+  readonly name: string
+}
+
+// Expressions
+
+export const unaryOperators = ['+', '-'] as const
+export type UnaryOperator = typeof unaryOperators[number]
+
+export const binaryOperators = ['+', '-', '*', '/'] as const
+export type BinaryOperator = typeof binaryOperators[number]
+
+export interface UnaryExpression extends ASTNode {
+  readonly type: 'UnaryExpression'
+  readonly operator: UnaryOperator
+  readonly argument: Expression
+}
+
+export interface BinaryExpression extends ASTNode {
+  readonly type: 'BinaryExpression'
+  readonly operator: BinaryOperator
+  readonly left: Expression
+  readonly right: Expression
+}
+
+export interface PropertyAccess extends ASTNode {
+  readonly type: 'PropertyAccess'
+  readonly object: Expression
+  readonly property: Identifier
+}
+
+export interface Call extends ASTNode {
+  readonly type: 'Call'
+  readonly callee: Expression
+  readonly arguments: ArgumentList
+}
+
+export type ArgumentList = ReadonlyArray<Expression | Property>
+
+export interface Property extends ASTNode {
+  readonly type: 'Property'
+  readonly key: Identifier
+  readonly value: Expression
+}
+
+// Primitive Types
+
+export interface Number extends ASTNode {
+  readonly type: 'Number'
+  readonly value: number
+}
+
+export interface String extends ASTNode {
+  readonly type: 'String'
+  readonly parts: ReadonlyArray<string | Expression>
+}
+
+export interface Pattern extends ASTNode {
+  readonly type: 'Pattern'
+  readonly mode: 'serial' | 'parallel'
+  readonly children: ReadonlyArray<Step | Expression>
+}
+
+export interface Step extends ASTNode {
+  readonly type: 'Step'
+  readonly value: StepValue
+  readonly length?: Expression
+  readonly parameters: ArgumentList
+}
+
+export interface Curve extends ASTNode {
+  readonly type: 'Curve'
+  readonly children: ReadonlyArray<CurveSegment | Expression>
+}
+
+export interface CurveSegment extends ASTNode {
+  readonly type: 'CurveSegment'
+  readonly curveType: string
+  readonly parameters: readonly Expression[]
+  readonly length: Expression
+}
+
+// Constructed Types
 
 export interface Mixer extends ASTNode {
   readonly type: 'Mixer'
@@ -154,18 +234,6 @@ export interface Part extends ASTNode {
   readonly children: readonly Statement[]
 }
 
-export interface Routing extends ASTNode {
-  readonly type: 'Routing'
-  readonly destination: Identifier
-  readonly source: Expression
-}
-
-export interface Automation extends ASTNode {
-  readonly type: 'Automation'
-  readonly target: Expression
-  readonly curve: Expression
-}
-
 export interface Instrument extends ASTNode {
   readonly type: 'Instrument'
   readonly children: readonly Statement[]
@@ -179,63 +247,16 @@ export interface Voice extends ASTNode {
   }
 }
 
-// Root Type
+// Combination Types
 
-export interface Program extends ASTNode {
-  readonly type: 'Program'
-  readonly imports: readonly Import[]
-  readonly children: readonly Statement[]
+export interface Routing extends ASTNode {
+  readonly type: 'Routing'
+  readonly destination: Identifier
+  readonly source: Expression
 }
 
-// Factory
-
-export function make<T extends keyof NodeByType> (
-  type: T,
-  range: SourceRange,
-  props: Omit<NodeByType[T], 'type' | 'range'>
-): NodeByType[T] {
-  return {
-    ...(props as any),
-
-    // must come last to override props
-    type,
-    range
-  } as NodeByType[T]
+export interface Automation extends ASTNode {
+  readonly type: 'Automation'
+  readonly target: Expression
+  readonly curve: Expression
 }
-
-export interface NodeByType {
-  Identifier: Identifier
-
-  Number: Number
-  String: String
-
-  Step: Step
-  Pattern: Pattern
-
-  CurveSegment: CurveSegment
-  Curve: Curve
-
-  Import: Import
-
-  UnaryExpression: UnaryExpression
-  BinaryExpression: BinaryExpression
-  PropertyAccess: PropertyAccess
-  Call: Call
-
-  Property: Property
-  Statement: Statement
-
-  Mixer: Mixer
-  Bus: Bus
-  Track: Track
-  Part: Part
-  Routing: Routing
-  Automation: Automation
-
-  Instrument: Instrument
-  Voice: Voice
-
-  Program: Program
-}
-
-export type AnyNode = NodeByType[keyof NodeByType]
